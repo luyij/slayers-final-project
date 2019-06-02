f4d673f1d9ef19d168e7bc8d23a048267b277d5f
Isabella Garcia <igarcia8@uw.edu>
2019-06-01 21:03:25 -0700

    created tabs from home, filter and help

--- ui.R
+++ ui.R
@@ -8,26 +8,42 @@
 #
 
 library(shiny)
+library(markdown)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("MovieHunter"),
   
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+  
+  navbarPage(img(src="exampleLogo.png", width = "45px"),
+    tabPanel("Home",
+        p("About the project page")
     ),
+    tabPanel("Filter",
+    # Sidebar with a slider input for number of bins 
+      sidebarLayout(
+        sidebarPanel(
+          #Filters the movies by genre
+          checkboxGroupInput("Genre", label = "Genre", 
+                         choices = list("Comedy" = 1, "Romance" = 2, "Drama" = 3)),
+          #Filters the movies by year
+          sliderInput("yearRange", label = "Year Range", min = 1916, 
+                         max = 2017, value = c(1916, 2017), sep = ""),
+          #Filters the movies by language options available
+          checkboxGroupInput("Language", label = "Language", 
+                         choices = list("English" = 1, "Spanish" = 2, "French" = 3))
+        ),
     
     # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
+      mainPanel(
+       # plotOutput("distPlot")
+      )
+      )
+    ),
+    tabPanel("Help",
+           p("About how to use the App")
     )
   )
 ))
