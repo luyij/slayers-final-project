d222ac2a1494b09adecf15dbc318a0f6f511f8f7
Isabella Garcia <igarcia8@uw.edu>
2019-06-03 16:57:29 -0700

    nothing

--- server.R
+++ server.R
@@ -58,7 +58,7 @@ shinyServer(function(input, output) {
     if(nrow(x())==0){}
     else{
       p <- ggplot() +
-        geom_point(data = x(), aes(x=year, y=imdb_score, key = title)) +
+        geom_point(data = x(), aes(x=year, y=imdb_score, color = ~content_rating)) +
         theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
               panel.background = element_blank(), axis.line = element_line(colour = "black")) +
         labs(title = "IMDB Score", x = "Year", y ="IMDB Scores")
